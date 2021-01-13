ffaaf03a205b487697501268ddd147227814bdd8
54696d654a6f6c74 <56564023+54696d654a6f6c74@users.noreply.github.com>
2021-01-13 19:04:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const QUOTES = [
     "\"DPS died at 2nd phase? No problem, time to turn healer mana into boss damage\" - Corns",
     "\"Are these healers saving mana for next expansion?\" - Cetone",
     "\"Wait there's shoulder enchants in this expansion?\" - Corns",
-    "\"We use markers on Malkarok?\" - Triama 1 year into SoO HC farm"
+    "\"We use markers on Malkarok?\" - Triama 1 year into SoO HC farm",
+    "\"I'm sorry but your lack of testicular mass concerns me\" - Corns"
 ]
 
 let date = new Date();
